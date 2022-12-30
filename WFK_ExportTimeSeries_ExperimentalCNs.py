4e381d6ffb6e587cf02f4cf9c7226d7f764db7f2
PaigeStork <90328689+PaigeStork@users.noreply.github.com>
2022-12-30 17:48:33 -0600

    Add files via upload

--- WFK_ExportTimeSeries_ExperimentalCNs.py
+++ WFK_ExportTimeSeries_ExperimentalCNs.py
@@ -1,5 +1,5 @@
-# name=CC_ExportTimeSeries_ExperimentalCNs
-# description=Export time series data of each HMS reach, which corresponds to a RAS key river station, to .txt files after running CC HMS models
+# name=WFK_ExportTimeSeries_ExperimentalCNs
+# description=Export time series data after running final version of WFK HMS model
 # displayinmenu=true
 # displaytouser=true
 # displayinselector=true
@@ -17,61 +17,63 @@ import sys
 #https://www.hec.usace.army.mil/confluence/dssjavaprogrammer/dss-progammers-guide-for-java
 #https://www.hec.usace.army.mil/software/hec-dssvue/documentation/User'sManual_2.0/HEC-DSSVue_20_Users_Manual.pdf
 
-reaches = ['MCC A - MCC B',
- 'MCC B MCC C',
- 'MCC C - MCC D',
- 'MCC D - MCC E',
- 'CC 17 - JUNCTION',
- 'CC16 - JUNCTION',
- 'TC B REACH 1',
- 'CC15 - JUNCTION',
- 'TC B REACH 2',
- 'CC 41 - JUNCTION',
- 'TC B REACH 3',
- 'CC 14 - JUNCTION',
- 'TC B REACH 4',
- 'CC23 - JUNCTION',
- 'CC24 - JUNCTION',
- 'JUNCTION - JUNCTION TC A',
- 'CC25 - JUNCTION',
- 'JUNCTION - TC A',
- 'CC21 - TC A',
- 'TC A - TC C',
- 'TC B - D',
- 'JUNCTION - TC F',
- 'CC53 - TC E',
- 'JUNCTION - TC G',
- 'CC31 - CC29',
- 'CC29 - UCC A',
- 'CC33 - UCC A',
- 'UCC A - B',
- 'UCC B - C',
- 'UCC C - JUNCTION',
- 'CC35 - JUNCTION',
- 'JUNCTION - UCC D',
- 'UCC & TC - JUNCTION',
- 'MCC H REACH 1',
- 'MCC E - MCC F',
- 'MCC H REACH 2',
- 'MCC H REACH 3',
- 'MCC I & H - MCC K',
- 'MCC O UPPER',
- 'Junction - MCC O',
- 'MCC N - MCC P',
- 'JUNCTION - LCC B',
- 'LCC B - D',
- 'LCC C - JUNCTION',
- 'JUNCTION - LCC F',
- 'JUNCTION - LCC G',
- 'LCC G - LCC H',
- 'LCC H - JUNCTION',
- 'Lower CC I INTO J',
- 'JUNCTION - LCC J',
- 'LCC A - LCC C']
+
+reaches = ['HC A - HC B',
+ 'HC B- HC C',
+ 'HC C - JUNCTION',
+ 'HC D - JUNCTION',
+ 'HC C&D - HC E',
+ 'WFK 17 - JUNCTION',
+ 'B A - JUNCTION',
+ 'B A & WFK 17 - B B',
+ 'B B - B C',
+ 'WFK 4 - JUNCTION',
+ 'WFK 5 - JUNCTION',
+ 'WFK 12 - KC WFK L',
+ 'WFK 3 - KC WFK F',
+ 'KLINKNER - JUNCTION',
+ 'MLSNA - JUNCTION',
+ 'JUNCTION - KC WFK A',
+ 'KC WFK A - KC WFK B',
+ 'WFK 1 - JUNCTION',
+ 'KC WFK C - JUNCTION',
+ 'JUNCTION - KC WFK D',
+ 'KC WFK D - JUNCTION',
+ 'KC WFK B - JUNCTION',
+ 'JUNCTION - KC WFK E',
+ 'WFK16 - KC WFK E',
+ 'KC WFK E - JUNCTION',
+ 'KC WFK F - JUNCTION',
+ 'JUNCTION - JUNCTION',
+ 'JUNCTION - KC WFK H',
+ 'KC WFK H & I - JUNCTION',
+ 'KC WFK L - JUNCTION',
+ 'JUNCTION- JUNCTION (KCWFK K)',
+ 'KC WFK J - JUNCTION',
+ 'JUNCTION 2- JUNCTION 3',
+ 'KC WFK M - JUNCTION',
+ 'JUNCTION - KC WFK K',
+ 'JUNCTION - SB',
+ 'SEAS & KC WFK - WFK B',
+ 'WFK A - JUNCTION',
+ 'WFK B JUNCTIONS',
+ 'WFK D - JUNCTION',
+ 'WFK B - JUNCTION',
+ 'WFK C - JUNCTION',
+ 'JUNCTION - WFK E',
+ 'WFK F - JUNCTION',
+ 'JUNCTION - WFK G',
+ 'B C - B D',
+ 'JUNCTION - JUNCTION - WFK H',
+ 'WFK I - JUNCTION',
+ 'JUNCTION - WFK H',
+ 'WFK H - JUNCTION',
+ 'HC E  - JUNCTION',
+ 'JUNCTION - OUTFLOW']
 
 
 #2yr
-file = "C:/Users/paige/OneDrive/Documents/HMS_CC_Final/CC_2yrStorm/MSE4_No_Dams.dss" # specify the DSS file
+file = "C:/Users/paige/OneDrive/Documents/HMS_WFK_Final/WFK_2yrStorm/MSE4_No_Dams.dss" # specify the DSS file
 dssfile = HecDss.open(file) # open the file
 #pull time series data into .txt file for each HMS reach
 for reach in range(len(reaches)):
@@ -81,12 +83,13 @@ for reach in range(len(reaches)):
 	theTable.showTable() # show the table (script won't run without this command; would rather tables visibility be muted)
 	opts = TableExportOptions()# get new export options
 	opts.delimiter = ","# delimit with commas
-	fileName = "C:/Users/paige/OneDrive/Documents/HMS_CC_Final/TimeSeriesData/2yr_{}.txt".format(reaches[reach]) # set the output file name
+	fileName = "C:/Users/paige/OneDrive/Documents/HMS_WFK_Final/TimeSeriesData/2yr_{}.txt".format(reaches[reach]) # set the output file name
 	theTable.export(fileName, opts)# export to the file
 dssfile.close() 
 
+
 #5yr
-file = "C:/Users/paige/OneDrive/Documents/HMS_CC_Final/CC_5yrStorm/MSE4_No_Dams.dss" 
+file = "C:/Users/paige/OneDrive/Documents/HMS_WFK_Final/WFK_5yrStorm/MSE4_No_Dams.dss" 
 dssfile = HecDss.open(file) # open the file
 #pull time series data into .txt file for each HMS reach
 for reach in range(len(reaches)):
@@ -96,12 +99,12 @@ for reach in range(len(reaches)):
 	theTable.showTable() # show the table (script won't run without this command; would rather tables visibility be muted)
 	opts = TableExportOptions()# get new export options
 	opts.delimiter = ","# delimit with commas
-	fileName = "C:/Users/paige/OneDrive/Documents/HMS_CC_Final/TimeSeriesData/5yr_{}.txt".format(reaches[reach]) # set the output file name
+	fileName = "C:/Users/paige/OneDrive/Documents/HMS_WFK_Final/TimeSeriesData/5yr_{}.txt".format(reaches[reach]) # set the output file name
 	theTable.export(fileName, opts)# export to the file
 dssfile.close() 
 
 #10yr
-file = "C:/Users/paige/OneDrive/Documents/HMS_CC_Final/CC_10yrStorm/MSE4_No_Dams.dss" 
+file = "C:/Users/paige/OneDrive/Documents/HMS_WFK_Final/WFK_10yrStorm/MSE4_No_Dams.dss" 
 dssfile = HecDss.open(file) # open the file
 #pull time series data into .txt file for each HMS reach
 for reach in range(len(reaches)):
@@ -111,12 +114,12 @@ for reach in range(len(reaches)):
 	theTable.showTable() # show the table (script won't run without this command; would rather tables visibility be muted)
 	opts = TableExportOptions()# get new export options
 	opts.delimiter = ","# delimit with commas
-	fileName = "C:/Users/paige/OneDrive/Documents/HMS_CC_Final/TimeSeriesData/10yr_{}.txt".format(reaches[reach]) # set the output file name
+	fileName = "C:/Users/paige/OneDrive/Documents/HMS_WFK_Final/TimeSeriesData/10yr_{}.txt".format(reaches[reach]) # set the output file name
 	theTable.export(fileName, opts)# export to the file
 dssfile.close() 
 
 #25yr
-file = "C:/Users/paige/OneDrive/Documents/HMS_CC_Final/CC_25yrStorm/MSE4_No_Dams.dss" 
+file = "C:/Users/paige/OneDrive/Documents/HMS_WFK_Final/WFK_25yrStorm/MSE4_No_Dams.dss" 
 dssfile = HecDss.open(file) # open the file
 #pull time series data into .txt file for each HMS reach
 for reach in range(len(reaches)):
@@ -126,12 +129,12 @@ for reach in range(len(reaches)):
 	theTable.showTable() # show the table (script won't run without this command; would rather tables visibility be muted)
 	opts = TableExportOptions()# get new export options
 	opts.delimiter = ","# delimit with commas
-	fileName = "C:/Users/paige/OneDrive/Documents/HMS_CC_Final/TimeSeriesData/25yr_{}.txt".format(reaches[reach]) # set the output file name
+	fileName = "C:/Users/paige/OneDrive/Documents/HMS_WFK_Final/TimeSeriesData/25yr_{}.txt".format(reaches[reach]) # set the output file name
 	theTable.export(fileName, opts)# export to the file
 dssfile.close() 
 
 #50yr
-file = "C:/Users/paige/OneDrive/Documents/HMS_CC_Final/CC_50yrStorm/MSE4_No_Dams.dss" 
+file = "C:/Users/paige/OneDrive/Documents/HMS_WFK_Final/WFK_50yrStorm/MSE4_No_Dams.dss" 
 dssfile = HecDss.open(file) # open the file
 #pull time series data into .txt file for each HMS reach
 for reach in range(len(reaches)):
@@ -141,12 +144,12 @@ for reach in range(len(reaches)):
 	theTable.showTable() # show the table (script won't run without this command; would rather tables visibility be muted)
 	opts = TableExportOptions()# get new export options
 	opts.delimiter = ","# delimit with commas
-	fileName = "C:/Users/paige/OneDrive/Documents/HMS_CC_Final/TimeSeriesData/50yr_{}.txt".format(reaches[reach]) # set the output file name
+	fileName = "C:/Users/paige/OneDrive/Documents/HMS_WFK_Final/TimeSeriesData/50yr_{}.txt".format(reaches[reach]) # set the output file name
 	theTable.export(fileName, opts)# export to the file
 dssfile.close() 
 
 #100yr
-file = "C:/Users/paige/OneDrive/Documents/HMS_CC_Final/CoonCreek_100yrStorm/MSE4_No_Dams.dss" 
+file = "C:/Users/paige/OneDrive/Documents/HMS_WFK_Final/WFK_100yrStorm/MSE4_No_Dams.dss" 
 dssfile = HecDss.open(file) # open the file
 #pull time series data into .txt file for each HMS reach
 for reach in range(len(reaches)):
@@ -156,12 +159,12 @@ for reach in range(len(reaches)):
 	theTable.showTable() # show the table (script won't run without this command; would rather tables visibility be muted)
 	opts = TableExportOptions()# get new export options
 	opts.delimiter = ","# delimit with commas
-	fileName = "C:/Users/paige/OneDrive/Documents/HMS_CC_Final/TimeSeriesData/100yr_{}.txt".format(reaches[reach]) # set the output file name
+	fileName = "C:/Users/paige/OneDrive/Documents/HMS_WFK_Final/TimeSeriesData/100yr_{}.txt".format(reaches[reach]) # set the output file name
 	theTable.export(fileName, opts)# export to the file
 dssfile.close() 
 
 #200yr
-file = "C:/Users/paige/OneDrive/Documents/HMS_CC_Final/CC_200yrStorm/MSE4_No_Dams.dss" 
+file = "C:/Users/paige/OneDrive/Documents/HMS_WFK_Final/WFK_200yrStorm/MSE4_No_Dams.dss" 
 dssfile = HecDss.open(file) # open the file
 #pull time series data into .txt file for each HMS reach
 for reach in range(len(reaches)):
@@ -171,12 +174,12 @@ for reach in range(len(reaches)):
 	theTable.showTable() # show the table (script won't run without this command; would rather tables visibility be muted)
 	opts = TableExportOptions()# get new export options
 	opts.delimiter = ","# delimit with commas
-	fileName = "C:/Users/paige/OneDrive/Documents/HMS_CC_Final/TimeSeriesData/200yr_{}.txt".format(reaches[reach]) # set the output file name
+	fileName = "C:/Users/paige/OneDrive/Documents/HMS_WFK_Final/TimeSeriesData/200yr_{}.txt".format(reaches[reach]) # set the output file name
 	theTable.export(fileName, opts)# export to the file
 dssfile.close() 
 
 #500yr
-file = "C:/Users/paige/OneDrive/Documents/HMS_CC_Final/CC_500yrStorm/MSE4_No_Dams.dss" 
+file = "C:/Users/paige/OneDrive/Documents/HMS_WFK_Final/WFK_500yrStorm/MSE4_No_Dams.dss" 
 dssfile = HecDss.open(file) # open the file
 #pull time series data into .txt file for each HMS reach
 for reach in range(len(reaches)):
@@ -186,6 +189,6 @@ for reach in range(len(reaches)):
 	theTable.showTable() # show the table (script won't run without this command; would rather tables visibility be muted)
 	opts = TableExportOptions()# get new export options
 	opts.delimiter = ","# delimit with commas
-	fileName = "C:/Users/paige/OneDrive/Documents/HMS_CC_Final/TimeSeriesData/500yr_{}.txt".format(reaches[reach]) # set the output file name
+	fileName = "C:/Users/paige/OneDrive/Documents/HMS_WFK_Final/TimeSeriesData/500yr_{}.txt".format(reaches[reach]) # set the output file name
 	theTable.export(fileName, opts)# export to the file
 dssfile.close() 
